860145baa39ef50ba32859d57bcc68c736fd57cb
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-11-02 19:38:03 +0530

    update product

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import config from "config";
 import mongoose from "mongoose";
 
 export const initDb = async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     await mongoose.connect(config.get("database.url"));
 };
